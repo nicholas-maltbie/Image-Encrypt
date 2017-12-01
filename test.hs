4190b8af291a20ea22796246442369765672c79e
Nick Maltbie <nick.dmalt@gmail.com>
2017-12-01 12:20:51 -0500

    got reading red channels

--- test.hs
+++ test.hs
@@ -2,8 +2,11 @@ import Codec.Picture
 import Data.Vector
 import qualified Data.Vector.Storable as V
 
-test :: Image a -> Int -> Int -> V.Vector (PixelBaseComponent a) -> IO ()
-test img w h vec = print (V.length vec)
+getRed :: PixelRGB8 -> Pixel8
+getRed (PixelRGB8 r g b) = r
+
+test :: Image PixelRGB8 -> IO ()
+test img = print (getRed (pixelAt img 0 0))
 
 -- main function 
 main :: IO ()
@@ -27,18 +30,19 @@ main = do
   -- load image and do stuff
   case imageLoad of
     Left error  -> putStrLn error
-    Right (ImageY8      image@(Image w h v))  -> putStrLn "Y8"
-    Right (ImageY16     image@(Image w h v))  -> putStrLn "Y16"
-    Right (ImageYA8     image@(Image w h v))  -> putStrLn "YA8"
-    Right (ImageYA16    image@(Image w h v))  -> putStrLn "YA16"
-    Right (ImageCMYK8   image@(Image w h v))  -> putStrLn "CMYK8"
-    Right (ImageYCbCr8  image@(Image w h v))  -> test image w h v--putStrLn "YCbCr8"
-    Right (ImageRGB8    image@(Image w h v))  -> putStrLn "RGB8"
-    Right (ImageRGBA8   image@(Image w h v))  -> putStrLn "RGBA8"
-    Right (ImageRGB16   image@(Image w h v))  -> putStrLn "RGB16"
-    Right (ImageRGBA16  image@(Image w h v))  -> putStrLn "RGBA16"
+    Right image -> test (convertRGB8 image)
+    --Right (ImageY8      image)  -> putStrLn "Y8"
+    --Right (ImageY16     image)  -> putStrLn "Y16"
+    --Right (ImageYA8     image)  -> putStrLn "YA8"
+    --Right (ImageYA16    image)  -> putStrLn "YA16"
+    --Right (ImageCMYK8   image)  -> putStrLn "CMYK8"
+    --Right (ImageYCbCr8  image)  -> putStrLn "CbCr8"
+    --Right (ImageRGB8    image)  -> putStrLn "RGB8"
+    --Right (ImageRGBA8   image)  -> putStrLn "RGBA8"
+    --Right (ImageRGB16   image)  -> putStrLn "RGB16"
+    --Right (ImageRGBA16  image)  -> putStrLn "RGBA16"
     
-    Right _ -> putStrLn "Unxexpected Pixel Format"
+    --Right _ -> putStrLn "Unxexpected Pixel Format"
     
   -- get name of output file
   putStrLn "Give name of output jpg file"
