3585b88f6a157d657cb4e936f905f92a57d27a65
Nick Maltbie <nick.dmalt@gmail.com>
2017-12-03 20:23:12 -0500

    Planned and started implemntation of LSB Stenography

--- test.hs
+++ test.hs
@@ -1,21 +1,49 @@
 import Codec.Picture
 import Data.Vector
 import Data.Bits
+import Data.Word
+import qualified Data.ByteString.Lazy as BS
 import qualified Data.Vector.Storable as V
 
-getRed :: PixelRGB8 -> Pixel8
-getRed (PixelRGB8 r g b) = r
+-- convert file to word list
+fileToWord8 :: String -> IO [Word8]
+fileToWord8 fp = do
+    contents <- BS.readFile fp
+    return $ BS.unpack contents
 
-test :: Image PixelRGB8 -> IO ()
-test img = print (getChangedPixel (getRed (pixelAt img 0 0)) 0 False)
+-- get width of image
+getWidth  :: Image PixelRGB8 -> Int
+getWidth  (Image w h _) = w
 
---sets bit 
+getHeight :: Image PixelRGB8 -> Int
+getHeight (Image w h _) = h
+
+-- Functions to get channel values from pixels
+getRed   :: PixelRGB8 -> Pixel8
+getRed   (PixelRGB8 r g b) = r
+
+getGreen :: PixelRGB8 -> Pixel8
+getGreen (PixelRGB8 r g b) = g
+
+getBlue  :: PixelRGB8 -> Pixel8
+getBlue  (PixelRGB8 r g b) = b
+
+-- encrypt data into image
+encryptBits :: Image PixelRGB8 -> [Word8] -> IO ()
+encryptBits img bytes = print w
+  where 
+    b = 2 -- bits per channel
+    l = Prelude.length bytes
+    w = getWidth img
+    h = getHeight img
+
+-- sets bit 
 getChanged :: Int -> Int -> Bool -> Int
 getChanged num idx val 
     | val     = num .|. bit idx
     | not val = num .&. complement (bit idx)
 
---set pixel bit
+-- set pixel bit
 getChangedPixel :: Pixel8 -> Int -> Bool -> Pixel8
 getChangedPixel px idx val
     | val     = px .|. bit idx
@@ -31,8 +59,9 @@ main = do
   f <- getLine
   -- read data from file
   putStrLn ("Reading data from " Prelude.++ f)
-  d <- readFile f
+  d <- fileToWord8 f
   putStrLn "Finished reading data from file"
+  putStrLn ("Read " Prelude.++ (show (Prelude.length d)) Prelude.++ " bytes")
   
   -- read image to encode image into
   putStrLn "What image is being read into: "
@@ -43,7 +72,7 @@ main = do
   -- load image and do stuff
   case imageLoad of
     Left error  -> putStrLn error
-    Right image -> test (convertRGB8 image)
+    Right image -> encryptBits (convertRGB8 image) d
     --Right (ImageY8      image)  -> putStrLn "Y8"
     --Right (ImageY16     image)  -> putStrLn "Y16"
     --Right (ImageYA8     image)  -> putStrLn "YA8"
